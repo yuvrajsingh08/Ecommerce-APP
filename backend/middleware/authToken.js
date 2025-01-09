8785b6abb4ea2ad21095a6412e57cf99a651aec7
Yuvraj Singh <uvsingh9999@gmail.com>
2025-01-09 16:21:49 +0530

    testing

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -8,6 +8,7 @@ async function authToken(req,res,next){
         if(!token){
             return res.status(200).json({
                 message : "Please Login...!",
+                token : token,
                 error : true,
                 success : false
             })
